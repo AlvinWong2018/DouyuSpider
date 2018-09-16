b6081f1da1afac5f0c23dae116c1ce086cdddbc0
AlvinWong2018 <lzzxhhh@126.com>
2018-09-16 16:26:44 +0800

    push new scrapy project

---
+++ DouyuSpider/pipelines.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
+import os
+
+import scrapy
+from scrapy.pipelines.images import ImagesPipeline
+from DouyuSpider.settings import IMAGES_STORE as images_store
+
+
+class DouyuspiderPipeline(ImagesPipeline):
+
+    def get_media_requests(self, item, info):
+        image_link = item['imagelink']
+        yield scrapy.Request(image_link)
+    # def process_item(self, item, spider):
+    #     return item
+
+    def item_completed(self, results, item, info):
+        image_path = [x['path'] for ok, x in results if ok]
+        os.rename(images_store+image_path[0], images_store+item['nickname']+".jpg")
+
